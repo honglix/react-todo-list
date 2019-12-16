1ed0e5395377f566a3975af9449d4a55478eae0d
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-15 20:32:52 -0500

    setup TodoList component

---
+++ src/components/Todolist.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Todolist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            
+         };
+    }
+    render() {
+        return (
+            <div>
+                <h2>Todo List</h2>
+            </div>
+        );
+    }
+}
+
+export default Todolist;
