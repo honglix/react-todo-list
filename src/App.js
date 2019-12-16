f1c24e3bef8325ea6c2aaa4f98e2ef1e45f70b16
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-15 21:47:30 -0500

    Add Things Todo and Things Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Todolist from './components/Todolist'
 function App() {
   return (
     <div>
+      
       <Todolist></Todolist>
 
     </div>
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -4,38 +4,90 @@ class Todolist extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            list: [
+                {
+                    title: "homework",
+                    checked: true
+                },
+                {
+                    title: "goodday",
+                    checked: false
+                }
+            ]
         };
     }
 
-    addData = () => {
-        var tempList = this.state.list;
-        tempList.push(this.refs.title.value);
-        this.setState({
-            list: tempList
-        })
-        this.refs.title.value = "";
+    addData = (e) => {
+        if(e.keyCode == 13){
+            let title = this.refs.title.value;
+            var tempList = this.state.list;
+            tempList.push({
+                title: title,
+                checked: false
+            });
+
+            this.setState({
+                list: tempList
+            })
+            this.refs.title.value = "";
+        }
     }
 
-    removeDate = (key) => {
+    removeData = (key) => {
         var tempList = this.state.list;
-        tempList.splice(key,1);
+        tempList.splice(key, 1);
         this.setState({
             list: tempList
         })
+    }
+
+    checkboxChange = (key) => {
+        var tempList = this.state.list;
+        tempList[key].checked = !tempList[key].checked;
+        this.setState({
+            list: tempList
+        })
+
     }
     render() {
         return (
-            <div>
-                <h2>Todo List</h2>
-                <input ref="title" /> <button onClick={this.addData}>Add the todo+</button>
+            <div >
+                <header className="title">
+                    <h2>Todo List</h2>
+                    <input ref="title" onKeyUp={this.addData}/> <button onClick={this.addData}>Add the todo+</button>
+                </header>
                 <hr />
-                <ul className="list">
-                {
-                    this.state.list.map((value,key) => (
-                        <li key={key}> {value} <button onClick={this.removeDate.bind(this,key)}>Delete</button></li> 
-                    ))
-                }
+                <h2>Things to do</h2>
+                <ul>
+                    {
+                        this.state.list.map((value, key) => {
+                            if (!value.checked) {
+                                return (
+                                    <li key={key}>
+                                        <input type="checkbox" checked={value.checked} onChange={this.checkboxChange.bind(this,key)}/> {value.title}
+                                        <button onClick={this.removeData.bind(this,key)}>Delete</button>
+                                    </li>
+
+                                )
+                            }
+                        })
+                    }
+                </ul>
+                <hr />
+                <h2>Things Done</h2>
+                <ul>
+                    {
+                        this.state.list.map((value, key) => {
+                            if (value.checked) {
+                                return (
+                                    <li key={key}>
+                                        <input type="checkbox" checked={value.checked} onChange={this.checkboxChange.bind(this,key)}/> {value.title}
+                                        <button onClick={this.removeData.bind(this,key)}>Delete</button>
+                                    </li>
+                                )
+                            }
+                        })
+                    }
                 </ul>
             </div>
         );
